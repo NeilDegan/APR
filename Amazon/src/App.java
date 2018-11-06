79c0c6e7c036c4b6e7969bc4db6aa2cd818631c6
Lukas <Lukas@Lukas-Laptop>
2018-11-06 14:15:54 +0100

    f

--- Amazon/src/App.java
+++ Amazon/src/App.java
@@ -1,4 +1,16 @@
-sa
 public class App {
-
+	public static void main(String[] args)
+	{
+		Medienbibliothek bib = new Medienbibliothek();
+		
+		Film wolf = new Film("Scorsecci", "120min", false, true, "The Wolf of Wall Street", "Drama", "Ist gut", "2002")
+		bib.alleMedienAusgeben();
+		
+		System.out.println("");
+		System.out.println("--- alle VHS in der Liste ---");
+		for(VHS vhs : bib.alleVHSalsListe())
+		{
+			vhs.anzeigen();
+		}
+	}
 }
