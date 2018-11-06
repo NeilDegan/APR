d1501ee31a207d4b44f02e56e2a88daa1f2f2adb
Lukas <Lukas@Lukas-Laptop>
2018-11-06 13:07:14 +0100

    Filme forläufig fertig

--- Amazon/src/Filme.java
+++ Amazon/src/Filme.java
@@ -1,4 +1,45 @@
+public class Filme extends Medium {
 
-public class Filme {
+    private String regisseur;
+    private String spielzeit;
+    private boolean uhd;
+    private boolean hd;
 
+    public boolean isHd() {
+        return hd;
+    }
+
+    public void setHd(boolean hd) {
+        this.hd = hd;
+    }
+
+    public boolean isUhd() {
+        return uhd;
+    }
+
+    public void setUhd(boolean uhd) {
+        this.uhd = uhd;
+    }
+
+    public String getSpielzeit() {
+        return spielzeit;
+    }
+
+    public void setSpielzeit(String spielzeit) {
+        this.spielzeit = spielzeit;
+    }
+
+    public String getRegisseur() {
+        return regisseur;
+    }
+
+    public void setRegisseur(String regisseur) {
+        this.regisseur = regisseur;
+    }
+    
+    public void anzeigen()
+	{
+		super.anzeigen();
+		System.out.println("Wer ist der Regisseur -> " + this.getRegisseur() + " Was ist die laufzeit -> " + this.getSpielzeit() + " Ist  UHD verfügbar -> " + this.isUhd() + " Ist  HD verfügbar -> " + this.isHd());
+	}
 }
