9f3a3d63193529f2b72fae1945766604ddae4e69
Lukas <Lukas@Lukas-Laptop>
2018-11-06 14:52:27 +0100

    g

--- Amazon/src/App.java
+++ Amazon/src/App.java
@@ -5,7 +5,10 @@ public class App {
 		Medienbibliothek bib = new Medienbibliothek();
 		
 		Filme wolf = new Filme("Scorsecci", "120min", false, true, "The Wolf of Wall Street", "Drama", "Ist gut", "2002");
+		Filme star = new Filme("Lucas", "200min", false, false, "Star Wars VI", "Si-Fi", "Ist sehr gut", "1966");
+		
 		bib.mediumHinzufügen(wolf);
+		bib.mediumHinzufügen(star);
 		bib.alleMedienAusgeben();
 	}
 }
