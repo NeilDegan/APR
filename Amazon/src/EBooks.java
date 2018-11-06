e6b66df9a0c4a70da762821ba8884e5808842c34
Neil <Neil@DESKTOP-9BDDNH2>
2018-11-06 14:00:27 +0100

    EBooks

--- Amazon/src/EBooks.java
+++ Amazon/src/EBooks.java
@@ -1,46 +1,55 @@
 
 public class EBooks extends Medium {
 	
-	private boolean vollständigesSet;
-	private String entwicklerstudio;
-	private Konsolentyp konsole;
+	private String autor;
+	private int isbn;
+	private int seitenanzahl;
+	private int auflage;
 	
-	public Cartridge(String titel, double wert, String standort, boolean vollständigesSet, String entwicklerstudio,
-			Konsolentyp konsole) {
-		super(titel, wert, standort);
-		this.vollständigesSet = vollständigesSet;
-		this.entwicklerstudio = entwicklerstudio;
-		this.konsole = konsole;
-	}
+	public EBooks(String autor, int isbn, int seitenanzahl, int auflage, String titel, String genre, String kommentar, String erscheinungsdatum) {
+        super(titel, genre, kommentar, erscheinungsdatum);
+        this.autor = autor;
+        this.isbn = isbn;
+        this.seitenanzahl = seitenanzahl;
+        this.auflage = auflage;
+    }
 
-	public boolean isVollständigesSet() {
-		return vollständigesSet;
-	}
+		public String getAutor() {
+	        return autor;
+	    }
 
-	public void setVollständigesSet(boolean vollständigesSet) {
-		this.vollständigesSet = vollständigesSet;
-	}
+	    public void setAutor(String autor) {
+	        this.autor = autor;
+	    }
 
-	public String getEntwicklerstudio() {
-		return entwicklerstudio;
-	}
+	    public int getIsbn() {
+	        return isbn;
+	    }
 
-	public void setEntwicklerstudio(String entwicklerstudio) {
-		this.entwicklerstudio = entwicklerstudio;
-	}
+	    public void setIsbn(int isbn) {
+	        this.isbn = isbn;
+	    }
 
-	public Konsolentyp getKonsole() {
-		return konsole;
-	}
+	    public int getSeitenanzahl() {
+	        return seitenanzahl;
+	    }
 
-	public void setKonsole(Konsolentyp konsole) {
-		this.konsole = konsole;
-	}
+	    public void setSeitenanzahl(int seitenanzahl) {
+	        this.seitenanzahl = seitenanzahl;
+	    }
+
+	    public int getAuflage() {
+	        return auflage;
+	    }
+
+	    public void setAuflage(int auflage) {
+	        this.auflage = auflage;
+	    }
 	
-	public void anzeigen() //Überschreiben der Mutterklassenmethode
-	{
-		super.anzeigen();
-		System.out.println("[Cartridge] Ist das Set vollständig? -> " + this.isVollständigesSet() + " Entwicklerstudio -> " + this.getEntwicklerstudio() + " Konsolentyp -> " + this.getKonsole());
-	}
+	    @Override
+	    public void anzeigen()
+		{
+			System.out.println("EBook: Titel -> " + this.getTitel() + " Autor -> " + this.getAutor() + " Genre -> " + this.getGenre() + " Erscheinungsdatum -> " + this.getErscheinungsdatum() + " ISBN -> " + this.getIsbn() + " Seitenanzahl -> " + this.getSeitenanzahl() + " Auflage -> " + this.getAuflage() + " Kommentar -> " + this.getKommentar());
+		}
 	
 }
