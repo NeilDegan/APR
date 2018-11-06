81c5b19868bdd3731e624f22d62c5c33d9af5acb
Neil <Neil@DESKTOP-9BDDNH2>
2018-11-06 14:38:28 +0100

    app

--- Amazon/src/App.java
+++ Amazon/src/App.java
@@ -1,13 +1,11 @@
 public class App {
+
 	public static void main(String[] args)
 	{
 		Medienbibliothek bib = new Medienbibliothek();
 		
-		Filme wolf = new Filme("Scorsecci", "120min", false, true, "The Wolf of Wall Street", "Drama", "Ist gut", "2002")
+		Filme wolf = new Filme("Scorsecci", "120min", false, true, "The Wolf of Wall Street", "Drama", "Ist gut", "2002");
 		bib.mediumHinzufügen(wolf);
-		
-		for(Medien m:medienliste) {
-			m.anzeigen();
-		}
+		bib.alleMedienAusgeben();
 	}
 }
