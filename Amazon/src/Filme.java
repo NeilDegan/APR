b0419d41da71be319fd840385658dec2f94b1fc7
Lukas <Lukas@Lukas-Laptop>
2018-11-06 13:54:22 +0100

    iatz aber

--- Amazon/src/Filme.java
+++ Amazon/src/Filme.java
@@ -5,6 +5,14 @@ public class Filme extends Medium {
     private boolean uhd;
     private boolean hd;
 
+    public Filme(String regisseur, String spielzeit, boolean uhd, boolean hd, String titel, String genre, String kommentar, String erscheinungsdatum) {
+        super(titel, genre, kommentar, erscheinungsdatum);
+        this.regisseur = regisseur;
+        this.spielzeit = spielzeit;
+        this.uhd = uhd;
+        this.hd = hd;
+    }
+    
     public boolean isHd() {
         return hd;
     }
